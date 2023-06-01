76481ef46706008fc1128417090b0a7b83568e22
root <root@win10verse>
2023-06-01 16:05:48 -0500

    update knex

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -3,5 +3,6 @@ const knex = require("knex");
 const config = require("../knexfile");
 
 // add SSL
+// update knexfile.js
 const environment = process.env.NODE_ENV || "development";
 module.exports = knex(config[environment]);
