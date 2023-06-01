c4d9cbb1e39c0163dea9609922379dc1a6ae9261
root <root@win10verse>
2023-06-01 14:42:34 -0500

    add SSL

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -2,5 +2,6 @@ require("dotenv").config();
 const knex = require("knex");
 const config = require("../knexfile");
 
+// add SSL
 const environment = process.env.NODE_ENV || "development";
 module.exports = knex(config[environment]);
