6e0c7c744159cb570773a8534189b38a77835ea4
root <root@win10verse>
2023-06-01 11:07:09 -0500

    deploy for render testing

--- knexfile.js
+++ knexfile.js
@@ -30,7 +30,7 @@ module.exports = {
     ...sharedConfig,
     connection: process.env.TESTING_DATABASE_URL,
   },
-  // render
+  // render db
   production: {
     ...sharedConfig,
     connection: process.env.DATABASE_URL,
