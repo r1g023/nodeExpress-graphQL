470747be3672d8deb33570a8e236ffd19a16e6e2
root <root@win10verse>
2023-06-06 11:27:43 -0500

    seed test

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -2,5 +2,7 @@ require("dotenv").config();
 const knex = require("knex");
 const config = require("../knexfile");
 
+// seed test
+
 const environment = process.env.NODE_ENV || "development";
 module.exports = knex(config[environment]);
