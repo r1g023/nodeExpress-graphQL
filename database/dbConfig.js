1098ec98e50188f7bb74cb84220232be363368ae
root <root@win10verse>
2023-06-06 10:50:50 -0500

    add render db

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -2,7 +2,5 @@ require("dotenv").config();
 const knex = require("knex");
 const config = require("../knexfile");
 
-// add SSL
-
-const environment = process.env.NODE_ENV || "renderdb";
+const environment = process.env.NODE_ENV || "development";
 module.exports = knex(config[environment]);
