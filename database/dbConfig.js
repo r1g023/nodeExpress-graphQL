39e6336c771c3329ba20781339b244e9601936c4
r1g023 <limon1285@gmail.com>
2023-06-12 10:45:28 -0500

    remove dev

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -3,6 +3,6 @@ const knex = require("knex");
 const config = require("../knexfile");
 
 // seed test //
-// production
+// production /
 const environment = "production";
 module.exports = knex(config[environment]);
